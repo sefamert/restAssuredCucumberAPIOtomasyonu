81bb8e3124dbc5c5c63b2b3e898747a83a50aaee
sefa.demiratli <sefa.demiratli@obss.tech>
2023-11-13 10:59:23 +0300

    test2

--- src/test/java/com/otelRezervasyonu/baseTest.java
+++ src/test/java/com/otelRezervasyonu/baseTest.java
@@ -16,7 +16,7 @@ public class baseTest
     body.put("totalprice",444);
     body.put("depositpaid",false);
 
-
+//test test
     JSONObject bookindDates = new JSONObject();
     bookindDates.put("checkin","2023-02-03");
     bookindDates.put("checkout","2023-04-06");
